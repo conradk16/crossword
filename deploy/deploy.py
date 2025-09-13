9c922f5d2dd17e7f4e4b878eb9b04b6638f66e30
conradk16 <conradkuklinsky@gmail.com>
2025-09-13 12:42:28 -0700

    simplify docker-compose to be shared between dev and prod

--- deploy/deploy.py
+++ deploy/deploy.py
@@ -2,24 +2,12 @@ import sys, os, re, paramiko, io
 from pathlib import Path
 from scp import SCPClient
 
-def update_docker_compose_with_sha(docker_compose_filepath, sha):
-    print("Updating local docker compose with sha...")
-    with open(docker_compose_filepath, 'r') as docker_compose_file:
-        docker_compose_content = docker_compose_file.read()
-    
-    # Replace anything that matches r':[a-f0-9]{7}' with the new sha
-    updated_docker_compose_content = re.sub(r':[a-f0-9]{7}', ":" + sha, docker_compose_content)
-
-    with open(docker_compose_filepath, 'w') as file:
-        file.write(updated_docker_compose_content)
-
 def get_ssh_connection(ip):
     key_path = os.getenv("EC2_KEY_PATH")
     if not key_path:
         raise RuntimeError("EC2_KEY_PATH environment variable is not set.")
 
     username = "ec2-user"
-
     key = paramiko.RSAKey.from_private_key_file(key_path)
 
     ssh = paramiko.SSHClient()
@@ -27,13 +15,26 @@ def get_ssh_connection(ip):
     ssh.connect(hostname=ip, username=username, pkey=key)
     return ssh
 
-# copies docker-compose.yml from the current directory to ec2
-def copy_docker_compose(ssh_connection, docker_compose_filepath):
+# copies docker-compose.yml from the current directory to ec2, substituting in replacements
+# replacements (dict): Dictionary of {key: value} to replace {{key}} in file.
+def copy_docker_compose(ssh_connection, docker_compose_filepath, replacements):
     print("Copying docker-compose to ec2...")
-    local_file = docker_compose_filepath.split('/')[-1]
+
+    with open(docker_compose_filepath, "r") as f:
+        contents = f.read()
+
+    for key, value in replacements.items():
+        pattern = r"\{\{\s*" + re.escape(key) + r"\s*\}\}"
+        contents = re.sub(pattern, str(value), contents)
+
+    temp_filepath = "/tmp/docker-compose.yml"
+    with open(temp_filepath, "w") as f:
+        f.write(contents)
+
+    # Copy file to EC2
     remote_file = "/home/ec2-user/docker-compose.yml"
     with SCPClient(ssh_connection.get_transport()) as scp:
-        scp.put(local_file, remote_file)
+        scp.put(temp_filepath, remote_file)
 
 def run_remote_command(ssh_connection, command):
     print(f"Running command {command} on ec2...")
@@ -45,11 +46,11 @@ def run_remote_command(ssh_connection, command):
         print(stderr.read().decode())
         raise Exception(f"Command failed: {command}")
 
-def deploy(sha, ip, docker_compose_filepath):
-    update_docker_compose_with_sha(docker_compose_filepath, sha)
+def deploy(ip, docker_compose_replacements):
+    docker_compose_filepath = str(Path(__file__).resolve().parent) + '/docker-compose.yml'
 
     ssh_connection = get_ssh_connection(ip)
-    copy_docker_compose(ssh_connection, docker_compose_filepath)
+    copy_docker_compose(ssh_connection, docker_compose_filepath, docker_compose_replacements)
 
     run_remote_command(ssh_connection, "docker-compose down")
     run_remote_command(ssh_connection, "docker-compose up -d")
@@ -63,7 +64,6 @@ if __name__ == '__main__':
         print('usage: python3 deploy.py <dev/prod> <7_char_commit_sha>')
         quit()
     print(f'Deploying {sys.argv[1]}')
+    docker_compose_replacements = {'tier' : sys.argv[1], 'sha' : sys.argv[2]}
     ec2_ip = '98.82.151.44' if sys.argv[1] == 'prod' else '54.158.16.33'
-    parent_filepath = str(Path(__file__).resolve().parent)
-    docker_compose_filepath = parent_filepath + f'/docker-compose-{sys.argv[1]}.yml'
-    deploy(sys.argv[2], ec2_ip, docker_compose_filepath)
+    deploy(ec2_ip, docker_compose_replacements)
