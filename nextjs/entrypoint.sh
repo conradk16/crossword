50e942726d021ff4d2a33212836a8e15d2872d61
conradk16 <conradkuklinsky@gmail.com>
2025-09-13 21:05:29 -0700

    fix bugs with entrypoint.sh

--- nextjs/entrypoint.sh
+++ nextjs/entrypoint.sh
@@ -15,13 +15,13 @@ DB_USER=$(echo "${SECRET_JSON}" | jq -r .username)
 DB_PASSWORD=$(echo "${SECRET_JSON}" | jq -r .password)
 DB_HOST=$(echo "${SECRET_JSON}" | jq -r .host)
 DB_PORT=$(echo "${SECRET_JSON}" | jq -r .port)
-DB_NAME=$(echo "${SECRET_JSON}" | jq -r .dbname)
+DB_NAME=$(echo "${SECRET_JSON}" | jq -r .database)
 
 # Construct and export the DATABASE_URL
 export DATABASE_URL="postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?sslmode=disable"
 
 echo "Waiting for database to be ready..."
-until atlas schema apply -u "$DATABASE_URL" --dry-run > /dev/null 2>&1; do
+until atlas schema apply -u "$DATABASE_URL" -f file:///app/db/schema.pg.hcl --dry-run; do
   >&2 echo "Postgres is unavailable - sleeping"
   sleep 1
 done
