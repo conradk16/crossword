d2db773f41225b0778d57800bb545c79ec09ee68
conradk16 <conradkuklinsky@gmail.com>
2025-09-16 15:20:19 -0700

    grab AWS secret for resend key in entrypoint.sh

--- nextjs/entrypoint.sh
+++ nextjs/entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 # --- Configuration ---
-SECRET_NAME="crossword-postgres"
+DB_SECRET_NAME="crossword-postgres"
 
 # Read the region from the environment variable
 : "${AWS_REGION:?AWS_REGION environment variable not set or empty}"
@@ -10,15 +10,21 @@ SECRET_NAME="crossword-postgres"
 echo "Fetching database credentials from AWS Secrets Manager in region: ${AWS_REGION}..."
 
 # Fetch the secret from AWS and parse it with jq
-SECRET_JSON=$(aws secretsmanager get-secret-value --secret-id "${SECRET_NAME}" --region "${AWS_REGION}" --query SecretString --output text)
-DB_USER=$(echo "${SECRET_JSON}" | jq -r .username)
-DB_PASSWORD=$(echo "${SECRET_JSON}" | jq -r .password)
-DB_HOST=$(echo "${SECRET_JSON}" | jq -r .host)
-DB_PORT=$(echo "${SECRET_JSON}" | jq -r .port)
-DB_NAME=$(echo "${SECRET_JSON}" | jq -r .database)
+DB_SECRET_JSON=$(aws secretsmanager get-secret-value --secret-id "${DB_SECRET_NAME}" --region "${AWS_REGION}" --query SecretString --output text)
+DB_USER=$(echo "${DB_SECRET_JSON}" | jq -r .username)
+DB_PASSWORD=$(echo "${DB_SECRET_JSON}" | jq -r .password)
+DB_HOST=$(echo "${DB_SECRET_JSON}" | jq -r .host)
+DB_PORT=$(echo "${DB_SECRET_JSON}" | jq -r .port)
+DB_NAME=$(echo "${DB_SECRET_JSON}" | jq -r .database)
 
 # Construct and export the DATABASE_URL
 export DATABASE_URL="postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?sslmode=disable"
+
+echo "Fetching Resend API key from AWS Secrets Manager..."
+RESEND_SECRET_NAME="resend_api_key"
+RESEND_SECRET_JSON=$(aws secretsmanager get-secret-value --secret-id "${RESEND_SECRET_NAME}" --region "${AWS_REGION}" --query SecretString --output text)
+RESEND_KEY_VALUE=$(echo "${RESEND_SECRET_JSON}" | jq -r .key)
+export RESEND_KEY="${RESEND_KEY_VALUE}"
 
 echo "Waiting for database to be ready..."
 until atlas schema apply -u "$DATABASE_URL" -f file:///app/db/schema.pg.hcl --dry-run; do
